d348d943a574f626584d4461412f87f24db2a541
noeli <noe.rmorales98@gmail.com>
2022-03-07 12:13:13 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GAC - Gestor de Ambiente Controlado',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
