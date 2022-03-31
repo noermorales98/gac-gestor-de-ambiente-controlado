edd722c39e2f660239e4b0a204f27ff8157e91ce
noeli <noe.rmorales98@gmail.com>
2022-03-30 22:17:50 -0500

    quedo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../services/service.service';
+import { map } from 'rxjs/Operators';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
-  constructor() {}
+  datos: any=[];
+
+  constructor(private datosService: ServiceService) {}
+  ngOnInit(): void {
+    this.retrieveDatos();
+  }
+
+  retrieveDatos(): void{
+    this.datosService.getAll().snapshotChanges().pipe(
+      map(changes=> changes.map(c=>({
+        key: c.payload.key, ...c.payload.val()}))
+        )
+    ).subscribe(data=>{
+      this.datos = data;
+      console.log(data);
+    })
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { ServiceService } from '../services/service.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
 
-  constructor() {}
+  datos:any[];
+
+  constructor(private datosService: ServiceService) {}
+  ngOnInit(): void {
+    this.recibir();
+  }
+
+  recibir(): void{
+    this.datosService.getAll().snapshotChanges().pipe(
+      map(changes=> changes.map(c=>({
+        key: c.payload.key, ...c.payload.val()}))
+        )
+    ).subscribe(data=>{
+      this.datos = data;
+      console.log(data);
+    })
+  }
 
 }
